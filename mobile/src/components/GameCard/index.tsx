66afd8e460d96fe4128b187c217c84a0a596c9d6
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2022-09-18 11:05:21 -0300

    listing integration with server

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -3,7 +3,6 @@ import {
   TouchableOpacity,
   TouchableOpacityProps,
   ImageBackground,
-  ImageSourcePropType,
   Text
 } from 'react-native';
 import { THEME } from '../../theme';
@@ -12,9 +11,11 @@ import { styles } from './styles';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    Ads: number
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -26,7 +27,7 @@ export function GameCard({ data, ...rest }: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
         style={styles.cover}
-        source={data.cover}
+        source={{ uri: data.bannerUrl }}
       >
 
         <LinearGradient
@@ -34,11 +35,11 @@ export function GameCard({ data, ...rest }: Props) {
           style={styles.footer}
         >
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
 
           <Text style={styles.ads}>
-            {data.ads}
+            {data._count.Ads}
           </Text>
 
         </LinearGradient>
